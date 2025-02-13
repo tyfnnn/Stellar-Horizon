28bc919e833081bfdfb38bdbc70099bef6c13832
tyfnnn <tayfun.i@icloud.com>
2025-02-13 09:05:37 +0100

    Day 3

---
+++ Stellar Horizon/Animations/RotatingEarthView2.swift
@@ -0,0 +1,18 @@
+//
+//  RotatingEarthView2.swift
+//  Stellar Horizon
+//
+//  Created by Tayfun Ilker on 11.02.25.
+//
+
+import SwiftUI
+
+struct RotatingEarthView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RotatingEarthView2()
+}
